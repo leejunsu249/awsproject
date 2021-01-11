2439f68cc2cffd51823663e71be6b07e5709b6d7
james.lee <75758585@naver.com>
2021-01-11 15:53:05 +0900

    Initial commit

---
+++ src/main/java/com/example/awsproj/awsproj/AwsprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsproj.awsproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awsproj/awsproj/AwsprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsproj.awsproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
